09f2ffb43a45bc006f9b727e8bffe02810bc2184
Lokesh <lokeshtrt0108@gmail.com>
2023-06-07 20:09:06 +0530

    Image added to the body section

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import Body from './Components/Body';
 //import { Component } from 'react';
 function App() {
   return (
     <div>
       <Navbar/>
+      <Body/>
     </div>
   );
 }
---
+++ src/Components/Body.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Body() {
+  return(
+    <section className = "Body">
+      <img src = {require('./KNOW YOUR STORE.png')} alt="KYS"  className = "body-image"/>
+    </section>
+  )
+}
